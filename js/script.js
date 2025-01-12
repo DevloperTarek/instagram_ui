4077b9d3f150dc8c590f7dc6c6a811ce03f8af12
Tarek Hossain <devlopertarek433@gmail.com>
2025-01-12 10:26:59 +0600

    header-start

---
+++ js/script.js
@@ -0,0 +1,45 @@
+// image file image preview
+document.getElementById('form-label').addEventListener('change',(event)=>{
+       const selected_files = event.target.files[0];
+      if(selected_files){
+              document.getElementById('file-name').innerText = `Files Name :: ${selected_files.name}`
+              const fileReader = new FileReader();
+              fileReader.onload = (e)=>{
+                     document.getElementById('preview-img').src = e.target.result;
+              }
+              fileReader.readAsDataURL(selected_files);
+      }else{
+              document.getElementById('preview-img').innerHTML = "Not preview image selected";
+      }
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// imgFile.addEventListener('change',(event) =>{
+//        const file = event.target.files[0];
+//        if(file){
+//               const fileReader = new FileReader(); //new file reader create
+//               fileReader.onload = (e) =>{
+//                      preview_img_adding.innerHTML = `<img src = "${e.target.result}" alt="preview image"/>`
+//               }
+//               fileReader.readAsDataURL(file);
+//        }else{
+//               preview_img_adding.innerHTML = "No image are selected";
+//        }
+// })
